1dbe1cd0c921fa200f5c80edd74bbaf7e6679b8f
esme518 <esme518@gmail.com>
2017-07-18 23:44:52 +0900

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ ! -d /git/oho-reader ]; then
+	mkdir /git
+	cd /git
+	git clone https://github.com/ShanaMaid/oho-reader.git
+	sed -i '/^app.listen(/{s/3001/PORT/}' /git/oho-reader/dist/app.js
+	sed -i "/^app.listen(/{s/PORT/$PORT/}" /git/oho-reader/dist/app.js
+	cd /git/oho-reader
+	npm install
+	npm run dist
+fi
+
+cd /git/oho-reader/dist
+
+exec node app.js
