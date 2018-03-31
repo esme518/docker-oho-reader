cc70342b151e9fa44a17b498344d2beb8ed7a774
chou <chou@debian-linux.localdomain>
2018-03-31 17:38:25 +0900

    Update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
 
 # Set environment
-if [ ! -d /git/oho-reader ]; then
-	mkdir /git
-	cd /git
-	git clone https://github.com/ShanaMaid/oho-reader.git
-	sed -i '/^app.listen(/{s/3001/PORT/}' /git/oho-reader/dist/app.js
-	sed -i "/^app.listen(/{s/PORT/$PORT/}" /git/oho-reader/dist/app.js
-	cd /git/oho-reader
-	npm install
-	npm run dist
-elif [ -n "$TEST" ]; then
-	cd /git/oho-reader
-	npm install
-	npm run dist
-fi
+sed -i "/^app.listen(/{s/PORT/$PORT/}" /git/oho-reader/dist/app.js
 
-cd /git/oho-reader/dist
-
-exec node app.js
+exec "$@"
