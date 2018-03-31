3c3612c476989149cd5f061de967dfb872457547
esme518 <esme518@gmail.com>
2018-03-31 17:53:41 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Set environment
-sed -i "/^app.listen(/{s/PORT/$PORT/}" /git/oho-reader/dist/app.js
+sed -i "/^app.listen(/{s/PORT/$PORT/}" /etc/git/oho-reader/dist/app.js
 
 exec "$@"
